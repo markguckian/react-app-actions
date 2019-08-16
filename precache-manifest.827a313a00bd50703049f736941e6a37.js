c0c2cc8e4c26ff80d46b4721600f6eb5f5221d7e
markguckian <markguckian@users.noreply.github.com>
2019-08-16 15:14:32 +0000

    Deploying to gh-pages - 15:14:32

---
+++ precache-manifest.827a313a00bd50703049f736941e6a37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21ada14058187463e2191d073dda966f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e5474f271af9ef1650fe",
+    "url": "./static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "79df8fc71e16ce128e56",
+    "url": "./static/js/2.5da17d27.chunk.js"
+  },
+  {
+    "revision": "e5474f271af9ef1650fe",
+    "url": "./static/js/main.9950743b.chunk.js"
+  },
+  {
+    "revision": "8e75e1eaa782a1e52fd5",
+    "url": "./static/js/runtime~main.e97a690c.js"
+  },
+  {
+    "revision": "4fce8d14995ebab50c8be34051d42fec",
+    "url": "./static/media/logo.4fce8d14.svg"
+  }
+]);
